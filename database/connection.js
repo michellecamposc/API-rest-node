27d29748861962bf25d90d961181f737d8c4fb32
michellecamposc <stephaniecampoos95@gmail.com>
2023-03-21 19:17:27 -0400

    Article model added

--- database/connection.js
+++ database/connection.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connection = async () => {
   try {
     await mongoose.connect("mongodb://localhost:27017/Blog");
-    console.log("Successfully connected to the my blog database");
+    console.log("Successfully connected with blog database");
   }
   catch (err) {
     console.log(err);
--- index.js
+++ index.js
@@ -1,8 +1,40 @@
 const { connection } = require("./database/connection")
 const express = require("express")
-
+const cors = require("cors");
+const res = require("express/lib/response");
 //Initializing app
 console.log("Initializing");
 
 //Connect to database
 connection();
+
+//Create Node server
+const app = express();
+const port = 3900;
+
+//Cors config
+app.use(cors());
+
+//Convert body to js object
+app.use(express.json());
+
+//Routes
+app.get("/testing", (req, res) => {
+  console.log("It has been executed on endpoint testing");
+
+  return res.status(200).json([{
+    title: "How to be a female programmer",
+    author: "Stephanie Campos"
+  },
+  {
+    title: "How to be a female programmer",
+    author: "Stephanie Campos"
+  }
+  ]);
+});
+
+//Create server and listen http requests
+app.listen(port, () => {
+  console.log(`Server running in the port ${port}`);
+})
+
